a010745f151234ce3af9a7dbd529fd2b5718ad99
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-13 20:29:02 +0100

    add apiinterface

---
+++ app/src/main/java/com/example/gotapp/api/APIInterface.kt
@@ -0,0 +1,27 @@
+package com.example.gotapp.api
+
+import com.example.gotapp.model.Data
+import okhttp3.OkHttpClient
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface APIInterface {
+    @GET("Characters")
+    suspend fun getCharacters(): Response<Data>
+
+    companion object {
+        private const val BASE_URL = "https://thronesapi.com/api/v2/"
+
+        fun create(): APIInterface {
+            val client = OkHttpClient.Builder().build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(APIInterface::class.java)
+        }
+    }
+}
