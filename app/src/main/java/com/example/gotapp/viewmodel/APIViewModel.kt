49cba721151a1327d02564af8d93c75d8eb3d3b6
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-14 00:16:21 +0100

    modify APIViewModel.kt

--- app/src/main/java/com/example/gotapp/viewmodel/APIViewModel.kt
+++ app/src/main/java/com/example/gotapp/viewmodel/APIViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.gotapp.viewmodel
 
-class APIViewModel {
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.gotapp.api.Repository
+import com.example.gotapp.model.Data
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class APIViewModel : ViewModel() {
+
+    private val repository = Repository()
+    private val _loading = MutableLiveData(true)
+    val loading = _loading
+    private val _characters = MutableLiveData<Data>()
+    val characters = _characters
+
+    fun getCharacters() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = repository.getAllCharacters()
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    _characters.value = response.body() as Data?
+                    _loading.value = false
+                } else {
+                    Log.e("Error :", response.message())
+                }
+            }
+        }
+    }
 }
