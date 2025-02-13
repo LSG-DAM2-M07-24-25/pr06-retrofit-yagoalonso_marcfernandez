9b654ee16a96ce53bf9c7f22bbb3715a3124155d
yagoalonso1 <yagoalonso3105@gmail.com>
2025-02-14 00:18:09 +0100

    mod:CharacterItem.kt

--- app/src/main/java/com/example/gotapp/view/CharacterItem.kt
+++ app/src/main/java/com/example/gotapp/view/CharacterItem.kt
@@ -1,2 +1,44 @@
 package com.example.gotapp.view
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.example.gotapp.model.Character
+
+@Composable
+fun CharacterItem(character: Character) {
+    Card(
+        border = BorderStroke(2.dp, Color.LightGray),
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            androidx.compose.foundation.Image(
+                painter = rememberImagePainter(character.imageUrl),
+                contentDescription = "Character Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(100.dp)
+            )
+            Text(
+                text = character.fullName,
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
