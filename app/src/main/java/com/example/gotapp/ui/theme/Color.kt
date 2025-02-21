574060bca0ba767765694b96b3e6bc2bc6746fd0
Marc Fernández <marcausente@gmail.com>
2025-02-21 16:41:17 +0100

    STYLE: Colors imported

--- app/src/main/java/com/example/gotapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/gotapp/ui/theme/Color.kt
@@ -2,6 +2,14 @@ package com.example.gotapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Colores inspirados en Game of Thrones
+val GotGold = Color(0xFFB39656)
+val GotDarkGray = Color(0xFF2A2A2A)
+val GotBlack = Color(0xFF1A1A1A)
+val GotBrown = Color(0xFF4A3C31)
+val GotLightGold = Color(0xFFD4B96A)
+val GotDarkRed = Color(0xFF8B0000)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
