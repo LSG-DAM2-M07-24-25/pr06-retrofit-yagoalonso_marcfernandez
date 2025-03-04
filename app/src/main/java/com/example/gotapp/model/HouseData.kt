9b7d645097c4a52fbfdbde151802dff1c28565c9
Marc Fernández <marcausente@gmail.com>
2025-03-04 01:55:43 +0100

    FEAT: Houses

---
+++ app/src/main/java/com/example/gotapp/model/HouseData.kt
@@ -0,0 +1,10 @@
+package com.example.gotapp.model
+
+data class HouseData(
+    val id: Int,
+    val name: String,
+    val motto: String,
+    val region: String,
+    val castle: String,
+    val imageUrl: String
+) 
